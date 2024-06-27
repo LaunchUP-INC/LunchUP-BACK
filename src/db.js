1879777cc674c1ee21c6136d1f15530ac7e05c34
polismai <polismai@gmail.com>
2024-06-26 21:47:47 -0300

    relaciones de modelos

--- src/db.js
+++ src/db.js
@@ -24,6 +24,17 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
+const { Dish, User, Child, Meal_Type } = sequelize.models;
+
+User.hasMany(Child, { through: "User_Child" });
+Child.belongsTo(User, { through: "User_Child" });
+Child.belongsToMany(Dish, { through: "Child_Dish" });
+Dish.belongsToMany(Child, { through: "Child_Dish" });
+
 module.exports = {
+  Dish,
+  User,
+  Child,
+  Meal_Type,
   conn: sequelize,
 };

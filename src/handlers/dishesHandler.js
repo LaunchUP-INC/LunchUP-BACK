5bef013cde5c145858b808f9e2fc70c193d9a672
unknown <tomas_toledo98@hotmail.com>
2024-06-28 20:50:49 -0300

    feature de prueba

--- src/handlers/dishesHandler.js
+++ src/handlers/dishesHandler.js
@@ -1,14 +1,20 @@
-const postDish = require("../controllers/postDish");
-const { getDishByName, getAllDishes } = require("../controllers/getDishes");
-const getDishById = require("../controllers/getDishById");
+const postDish = require('../controllers/postDish');
+const { getDishByName, getAllDishes } = require('../controllers/getDishes');
+const getDishById = require('../controllers/getDishById');
 
 const createDishesHandler = async (req, res) => {
   const { name, description, price, image, mealTypeId } = req.body;
   try {
-    const newId = await postDish({ name, description, price, image, mealTypeId });
+    const newId = await postDish({
+      name,
+      description,
+      price,
+      image,
+      mealTypeId,
+    });
     res.status(200).json({ newId });
   } catch (error) {
-    res.status(400).json({ error: error.message });  
+    res.status(400).json({ error: error.message });
   }
 };
 
@@ -20,11 +26,12 @@ const getDishesHandler = async (req, res) => {
       const dishByName = await getDishByName(name);
 
       if (!dishByName.length) {
-        return res.status(404).json({ error: 'No se encontraron platos de comida con ese nombre' });
+        return res
+          .status(404)
+          .json({ error: 'No se encontraron platos de comida con ese nombre' });
       }
 
       res.status(200).json({ dishByName });
-
     } else {
       const allDishes = await getAllDishes();
       res.status(200).json({ allDishes });
@@ -45,8 +52,36 @@ const getDetailHandler = async (req, res) => {
   }
 };
 
+const putDishesHandler = async (req, res) => {
+  const { id } = req.params;
+  const { name, description, price, image } = req.body;
+  try {
+    const response = await Dish.findByPk(id);
+    console.log(response);
+    res.status(200).json({ response });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteDishesHandler = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Dish.destroy({
+      where: {
+        id,
+      },
+    });
+    res.status(200).json(`El menú con ID ${id} ha sido eliminado`);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createDishesHandler,
   getDishesHandler,
-  getDetailHandler
-}
+  getDetailHandler,
+  putDishesHandler,
+  deleteDishesHandler,
+};
--- src/routes/dishesRouter.js
+++ src/routes/dishesRouter.js
@@ -1,11 +1,19 @@
-const { Router } = require("express");
-const { createDishesHandler, getDishesHandler, getDetailHandler } = require("../handlers/dishesHandler");
+const { Router } = require('express');
+const {
+  createDishesHandler,
+  getDishesHandler,
+  getDetailHandler,
+  putDishesHandler,
+  deleteDishesHandler,
+} = require('../handlers/dishesHandler');
 const { validate } = require('../utils');
 
 const dishesRouter = Router();
 
-dishesRouter.get("/:id", getDetailHandler);
-dishesRouter.get("/", getDishesHandler);
-dishesRouter.post("/", validate, createDishesHandler);
+dishesRouter.get('/:id', getDetailHandler);
+dishesRouter.get('/', getDishesHandler);
+dishesRouter.post('/', validate, createDishesHandler);
+dishesRouter.put('/:id', putDishesHandler);
+dishesRouter.delete('/:id', deleteDishesHandler);
 
 module.exports = dishesRouter;

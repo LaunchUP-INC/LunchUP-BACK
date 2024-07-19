6977eff646c0a9cf70a97cf6849685eb1fdf6991
Cintia Poloni <cintia.poloni@gmail.com>
2024-07-18 23:34:22 -0300

    modificacionesUser

--- src/scripts/populateReviewsTable.js
+++ src/scripts/populateReviewsTable.js
@@ -6,53 +6,53 @@ const populateReviews = async () => {
       {
         comment: "Excelente solucion para la merienda escolar de mis hijos",
         score: 5,
-        UserId: "8b195a37-5c26-40d9-8135-ae9fea6b3769",
+        UserId: "6daaf748-164e-4b5a-b8f9-03a8f29c0cae",
       },
 
       {
         comment:
           "Muy útil para gestionar las comidas escolares. mucha variedad de platos",
         score: 5,
-        UserId: "a9bf3931-84e4-4490-9ba2-735a8acfc1be",
+        UserId: "3ec06b64-86a0-4a01-b403-b6eb7761cecb",
       },
 
       {
         comment:
           "Excelente app. Puedo ver el menú del comedor escolar y planificar las comidas de mis hijos fácilmente.",
         score: 5,
-        UserId: "2f707d37-df73-4fe2-bda6-0ae8cfb15113",
+        UserId: "30ad56ff-dad5-421d-9678-631e5f02d040",
       },
 
       {
         comment:
           "Muy buena esta app. Puedo ver el menú escolar diario y gestionar los pagos sin problemas",
         score: 4,
-        UserId: "44d0cadb-49f6-4de6-acaf-ac8ace79f0d9",
+        UserId: "e8670d75-ac33-42d6-be5b-757a3e60586c",
       },
 
       {
         comment:
           "Recomiendo esta app, me encanta que puedo seleccionar que tipo de plato prefiero para mi hijo",
         score: 4,
-        UserId: "ab174d20-bf7d-4481-8bfc-479197af0b81",
+        UserId: "7d68ec0b-f6e0-45ad-89f8-0eff82f5eda4",
       },
 
       {
         comment: "Muy buena aplicacion, facil de usar",
         score: 4,
-        UserId: "4cdc43bc-9970-4a29-8474-d672fa7ae3a1",
+        UserId: "4f1f3b40-ff9e-4ced-b8f1-cd430980ff4b",
       },
       {
         comment:
           "¡App genial! Me ayuda a estar al tanto del menú escolar y gestionar los pagos de manera sencilla. ¡Muy útil!",
         score: 5,
-        UserId: "eb03ff4e-5b6b-46e6-afa8-225b61315895",
+        UserId: "526b22e6-3775-4da5-911e-55c0100739f9",
       },
       {
         comment:
           "Me facilita la merienda de mis hijos, puedo saber que esta consumiendo",
         score: 4,
-        UserId: "81b9cd9e-16a5-49bf-8ae0-42b716e7224c",
+        UserId: "93f629dc-2662-40b5-914d-190d6bd1ab6a",
       },
     ];
 

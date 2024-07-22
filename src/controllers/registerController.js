4d463c819c63e45b810338883658a40bcbbd5b59
unknown <tomas_toledo98@hotmail.com>
2024-07-22 00:15:29 -0300

    arreglo en formulario de registro

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -11,7 +11,6 @@ const registerUser = async (
   telephone,
   email,
   password,
-  isAdmin,
   banned
 ) => {
   const existingUser = await User.findOne({ where: { email } });
@@ -28,7 +27,6 @@ const registerUser = async (
     telephone,
     email,
     password: hash,
-    isAdmin,
     banned,
   });
 
